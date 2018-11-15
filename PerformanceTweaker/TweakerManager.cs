ac90a6f44d069b86e7d4f387a2d1748654ee7a47
Florian Kinder <florian.kinder@fankserver.com>
2018-11-15 12:14:32 +0100

    Fixes

--- PerformanceTweaker/TweakerManager.cs
+++ PerformanceTweaker/TweakerManager.cs
@@ -15,17 +15,17 @@ namespace PerformanceTweaker
         [Dependency] private readonly PatchManager _patchManager;
         private PatchContext _ctx;
         private static readonly ConcurrentDictionary<long, int> _largeTurretBaseSlowdown = new ConcurrentDictionary<long, int>();
+        private static readonly MethodInfo _largeTurretBaseUpdateAfterSimulation =
+            typeof(MyLargeTurretBase).GetMethod(nameof(MyLargeTurretBase.UpdateAfterSimulation), BindingFlags.Instance | BindingFlags.Public) ??
+            throw new Exception("Failed to find patch method");
         private static readonly MethodInfo _largeTurretBaseUpdateAfterSimulation10 =
             typeof(MyLargeTurretBase).GetMethod(nameof(MyLargeTurretBase.UpdateAfterSimulation10), BindingFlags.Instance | BindingFlags.Public) ??
             throw new Exception("Failed to find patch method");
-        private static readonly MethodInfo _largeTurretBaseUpdateAfterSimulation100 =
-            typeof(MyLargeTurretBase).GetMethod(nameof(MyLargeTurretBase.UpdateAfterSimulation100), BindingFlags.Instance | BindingFlags.Public) ??
+        private static readonly MethodInfo _largeTurretBaseThrottler =
+            typeof(TweakerManager).GetMethod(nameof(TweakerManager.LargeTurretBaseThrottler), BindingFlags.Static | BindingFlags.Public) ??
             throw new Exception("Failed to find patch method");
         private static readonly MethodInfo _largeTurretBaseThrottler10 =
             typeof(TweakerManager).GetMethod(nameof(TweakerManager.LargeTurretBaseThrottler10), BindingFlags.Static | BindingFlags.Public) ??
-            throw new Exception("Failed to find patch method");
-        private static readonly MethodInfo _largeTurretBaseThrottler100 =
-            typeof(TweakerManager).GetMethod(nameof(TweakerManager.LargeTurretBaseThrottler100), BindingFlags.Static | BindingFlags.Public) ??
             throw new Exception("Failed to find patch method");
 
         public static readonly Logger Log = LogManager.GetCurrentClassLogger();
@@ -40,8 +40,8 @@ namespace PerformanceTweaker
 
             if (_ctx == null)
                 _ctx = _patchManager.AcquireContext();
+            _ctx.GetPattern(_largeTurretBaseUpdateAfterSimulation).Prefixes.Add(_largeTurretBaseThrottler);
             _ctx.GetPattern(_largeTurretBaseUpdateAfterSimulation10).Prefixes.Add(_largeTurretBaseThrottler10);
-            _ctx.GetPattern(_largeTurretBaseUpdateAfterSimulation100).Prefixes.Add(_largeTurretBaseThrottler100);
             _patchManager.Commit();
         }
 
@@ -52,14 +52,14 @@ namespace PerformanceTweaker
             _patchManager.FreeContext(_ctx);
         }
 
+        public static bool LargeTurretBaseThrottler(MyLargeTurretBase __instance)
+        {
+            return LargeTurretBaseThrottler(__instance, 100);
+        }
+
         public static bool LargeTurretBaseThrottler10(MyLargeTurretBase __instance)
         {
             return LargeTurretBaseThrottler(__instance, 10);
-        }
-
-        public static bool LargeTurretBaseThrottler100(MyLargeTurretBase __instance)
-        {
-            return LargeTurretBaseThrottler(__instance, 100);
         }
 
         public static bool LargeTurretBaseThrottler(MyLargeTurretBase __instance, int tick)
@@ -68,12 +68,11 @@ namespace PerformanceTweaker
                 return true;
 
             int value = _largeTurretBaseSlowdown.AddOrUpdate(__instance.EntityId, 1, (key, oldValue) => oldValue++);
-            if (TweakerPlugin.Instance.Config.LargeTurretBaseTweakFactorType == 0 && value < (int)((TweakerPlugin.Instance.Config.LargeTurretBaseTweakFactor - Sync.ServerSimulationRatio) * tick))
+            if (TweakerPlugin.Instance.Config.LargeTurretBaseTweakFactorType == 0 && value < (int)(1 - (TweakerPlugin.Instance.Config.LargeTurretBaseTweakFactor - Sync.ServerSimulationRatio) * tick))
                 return false;
             else if (TweakerPlugin.Instance.Config.LargeTurretBaseTweakFactorType == 1
                 && Sync.ServerCPULoad - TweakerPlugin.Instance.Config.LargeTurretBaseTweakFactor > 0
-                && value < (1 - (Sync.ServerCPULoad / TweakerPlugin.Instance.Config.LargeTurretBaseTweakFactor)) * tick)
-            if (value < (int)((1 - Sync.ServerSimulationRatio) * 10))
+                && value < (TweakerPlugin.Instance.Config.LargeTurretBaseTweakFactor / Sync.ServerCPULoad) * tick)
                 return false;
             _largeTurretBaseSlowdown[__instance.EntityId] = 0;
 
