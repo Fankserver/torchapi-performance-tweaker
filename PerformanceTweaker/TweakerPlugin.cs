4e27641c4610fff2e762e432f40682a06555a777
Florian Kinder <florian.kinder@fankserver.com>
2018-11-15 22:13:27 +0100

    Need async patch

--- PerformanceTweaker/TweakerPlugin.cs
+++ PerformanceTweaker/TweakerPlugin.cs
@@ -1,10 +1,6 @@
 ﻿using NLog;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Controls;
 using Torch;
 using Torch.API;
