f173fc0e72e07498d9d389b33bbb820d78b9cd24
Florian Kinder <florian.kinder@fankserver.com>
2018-11-15 22:13:34 +0100

    O.o

--- PerformanceTweaker/TweakerManager.cs
+++ PerformanceTweaker/TweakerManager.cs
@@ -1,9 +1,12 @@
 ﻿using NLog;
 using PerformanceTweaker.Patch;
-using Sandbox.Game.Weapons;
+using System.Threading.Tasks;
 using Torch.API;
+using Torch.API.Managers;
+using Torch.API.Session;
 using Torch.Managers;
 using Torch.Managers.PatchManager;
+using Torch.Session;
 
 namespace PerformanceTweaker
 {
@@ -11,6 +14,7 @@ namespace PerformanceTweaker
     {
         [Dependency] private readonly PatchManager _patchManager;
         private PatchContext _ctx;
+        private TorchSessionManager _sessionManager;
 
         public static readonly Logger Log = LogManager.GetCurrentClassLogger();
 
@@ -22,13 +26,14 @@ namespace PerformanceTweaker
         {
             base.Attach();
 
+            _sessionManager = Torch.Managers.GetManager<TorchSessionManager>();
+            if (_sessionManager != null)
+                _sessionManager.SessionStateChanged += SessionChanged;
+            else
+                Log.Warn("No session manager loaded!");
+
             if (_ctx == null)
                 _ctx = _patchManager.AcquireContext();
-            //_ctx.GetPattern(MyLargeTurretBasePatch._updateAfterSimulation).Transpilers.Add(MyLargeTurretBasePatch._transpilerForAfterUpdate1);
-            //_ctx.GetPattern(MyLargeTurretBasePatch._updateAfterSimulation10).Transpilers.Add(MyLargeTurretBasePatch._transpilerForAfterUpdate10);
-            _ctx.GetPattern(MyLargeTurretBasePatch._updateAfterSimulation).Prefixes.Add(MyLargeTurretBasePatch._throttler1);
-            _ctx.GetPattern(MyLargeTurretBasePatch._updateAfterSimulation10).Prefixes.Add(MyLargeTurretBasePatch._throttler10);
-            _patchManager.Commit();
         }
 
         public override void Detach()
@@ -38,6 +43,24 @@ namespace PerformanceTweaker
             MyLargeTurretBasePatch.Close();
 
             _patchManager.FreeContext(_ctx);
+        }
+
+        private void SessionChanged(ITorchSession session, TorchSessionState state)
+        {
+            switch (state)
+            {
+                case TorchSessionState.Loaded:
+
+                    Task.Delay(3000).ContinueWith((t) =>
+                    {
+                        Log.Debug("Patching MyLargeTurretBasePatch");
+                        _ctx.GetPattern(MyLargeTurretBasePatch._updateAfterSimulation).Prefixes.Add(MyLargeTurretBasePatch._throttler1);
+                        _ctx.GetPattern(MyLargeTurretBasePatch._updateAfterSimulation10).Prefixes.Add(MyLargeTurretBasePatch._throttler10);
+                        _patchManager.Commit();
+                    });
+
+                    break;
+            }
         }
     }
 }
